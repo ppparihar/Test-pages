fcb706b991233d1a195dc4774f91d4d39e3765de
Prem Parihar <loveprem073@gmail.com>
2017-09-11 23:03:17 +0530

    configured travis

---
+++ scripts/ci/install.sh
@@ -0,0 +1,9 @@
+# Install all npm dependencies 
+travisFoldStart "npm-install"
+  npm install
+travisFoldEnd "npm-install"
+
+# Install bower packages
+travisFoldStart "bower-install"
+  bower install
+travisFoldEnd "bower-install"
