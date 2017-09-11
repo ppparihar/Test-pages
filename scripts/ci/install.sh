a2ba4ad94071a8d3941868f24ece716bf8ad4521
Prem Parihar <loveprem073@gmail.com>
2017-09-11 23:12:07 +0530

    added polymer cli

--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -2,7 +2,7 @@
 #travisFoldStart "npm-install"
   npm install
 #travisFoldEnd "npm-install"
-
+  npm install -g polymer-cli
 # Install bower packages
 #travisFoldStart "bower-install"
   bower install
