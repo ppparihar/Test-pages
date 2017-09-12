3ec9f2b4ca911e6f5d31563869bb8ca50b24cf1e
Travis CI <loveprem073@gmail.com>
2017-09-12 09:02:55 +0000

    Deploy to GitHub Pages: e97c98b53b2455a43e6a39e2b93c5d3fbf17c85f

--- scripts/ci/install.sh
+++
@@ -1,9 +0,0 @@
-# Install all npm dependencies 
-#travisFoldStart "npm-install"
-  npm install
-#travisFoldEnd "npm-install"
-  npm install -g polymer-cli
-# Install bower packages
-#travisFoldStart "bower-install"
-  bower install
-#travisFoldEnd "bower-install"
